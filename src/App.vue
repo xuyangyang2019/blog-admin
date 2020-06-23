16245280d136072c626e23cf429905a847a8c144
xuyangyang2019 <15268296911@163.com>
2020-06-23 10:49:24 +0800

    导入和更改样式

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "./styles/reset.scss";
+@import "./styles/common.scss";
+// @import "~@/assets/iconfont/iconfont.css";
+// @import "~@/assets/font-awesome/css/font-awesome.css";
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
+  height: 100%;
 }
+
 #nav {
   padding: 30px;
   a {
