7a4fb64e45047c5c5fb0455c49b3afb7d0c5defd
xuyangyang2019 <15268296911@163.com>
2020-06-24 18:48:29 +0800

    format

--- src/components/article/allArticles.vue
+++ src/components/article/allArticles.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="admin-articles">
-    <!-- <list :article_list="articles.all"></list> -->
+    <list :article_list="articles.all"></list>
   </div>
 </template>
 <script>
 import { mapActions, mapState } from "vuex";
-// import list from "@/components/article/articleList";
+import list from "@/components/article/articleList";
 export default {
   components: {
-    // list
+    list
   },
   //组件缓存后，为了让每个模块显示正确的页码，故重新计算页码数
   beforeRouteEnter(to, from, next) {
-    next((vm) => {
+    next(vm => {
       vm.getArticlesCount({ publish: true });
       document.title = "后台管理 -已发表文章";
     });
