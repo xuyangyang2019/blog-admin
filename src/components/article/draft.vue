9819350ad7ff11dd3f8866c5a57bf81217ffa81c
xuyangyang2019 <15268296911@163.com>
2020-06-29 17:40:08 +0800

    草稿箱页面

--- src/components/article/draft.vue
+++ src/components/article/draft.vue
@@ -1,44 +1,59 @@
 <template>
   <div class="draft">
+    {{ articles }}
     <list :article_list="articles.drafts"></list>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
-import list from "@/components/article/articleList";
+import { mapActions, mapGetters } from "vuex"
+
+import list from "@/components/article/articleList"
 
 export default {
-  components: {
-    list
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      vm.getArticlesCount({ publish: false });
-      document.title = "后台管理 -草稿箱";
-    });
-  },
-  created() {
-    this.allArticles_admin();
+  data() {
+    return {}
   },
   computed: {
-    ...mapState(["articles"])
+    ...mapGetters({
+      articles: "axios/articles"
+    })
   },
   methods: {
-    ...mapActions(["getArticles", "getArticlesCount"]),
+    ...mapActions({
+      getArticles: "axios/GetArticles",
+      getArticlesCount: "axios/GetArticlesCount"
+    }),
+    // 分页查询未发表的文章
     allArticles_admin: function() {
       let payload = {
         publish: false,
         page: 1
-      };
-      this.getArticles(payload);
+      }
+      this.getArticles(payload)
     }
+  },
+  components: {
+    list
+  },
+  created() {
+    console.log("created")
+    this.allArticles_admin()
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log("beforeRouteEnter")
+    next(vm => {
+      vm.getArticlesCount({ publish: false })
+      document.title = "后台管理 -草稿箱"
+    })
   }
-};
+}
 </script>
 
 <style lang="scss">
 .draft {
   margin-top: 15px;
+  border: solid red 1px;
+  color: black;
 }
 </style>
