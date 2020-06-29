a2ab676b0830814359a1be8a36561d3195148123
xuyangyang2019 <15268296911@163.com>
2020-06-29 12:29:13 +0800

    全局过滤器

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import filters from "./filters";
+
 Vue.config.productionTip = false;
+
+// 全局过滤器
+Object.keys(filters).forEach(filterName => {
+  Vue.filter(filterName, filters[filterName]);
+});
 
 new Vue({
   router,
