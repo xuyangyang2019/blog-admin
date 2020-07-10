ced7eb6ac1a4b0ef236d46d5cf7e3954eb42bbc3
xuyangyang2019 <15268296911@163.com>
2020-07-10 15:49:37 +0800

    重写vue-quill-edit的图片上传方法

--- src/components/admin/PublishArticle.vue
+++ src/components/admin/PublishArticle.vue
@@ -27,11 +27,11 @@ export default {
   data() {
     /*富文本编辑图片上传配置*/
     const uploadConfig = {
-      action: "", // 必填参数 图片上传地址
+      action: "http://www.jjldkj.com:10089/fileUpload", // 必填参数 图片上传地址
       methods: "POST", // 必填参数 图片上传方式
       token: "", // 可选参数 如果需要token验证，假设你的token有存放在sessionStorage
-      name: "img", // 必填参数 文件的参数名
-      size: 500, // 可选参数   图片大小，单位为Kb, 1M = 1024Kb
+      name: "myfile", // 必填参数 文件的参数名
+      size: 1024 * 20, // 可选参数   图片大小，单位为Kb, 1M = 1024Kb
       accept: "image/png, image/gif, image/jpeg, image/bmp, image/x-icon" // 可选 可上传的图片格式
     }
     const toolOptions = [
@@ -52,56 +52,62 @@ export default {
     ]
     const handlers = {
       image: function image() {
-        var self = this
-        var fileInput = this.container.querySelector("input.ql-image[type=file]")
+        let self = this
+        let fileInput = this.container.querySelector("input.ql-image[type=file]")
         if (fileInput === null) {
           fileInput = document.createElement("input")
           fileInput.setAttribute("type", "file")
           // 设置图片参数名
-          if (uploadConfig.name) {
-            fileInput.setAttribute("name", uploadConfig.name)
-          }
+          // if (uploadConfig.name) {
+          //   fileInput.setAttribute("name", uploadConfig.name)
+          // }
           // 可设置上传图片的格式
           fileInput.setAttribute("accept", uploadConfig.accept)
           fileInput.classList.add("ql-image")
           // 监听选择文件
           fileInput.addEventListener("change", function() {
-            console.log(fileInput.files[0])
             // 创建formData
-            // var formData = new FormData()
-            // formData.append(uploadConfig.name, fileInput.files[0])
+            let formData = new FormData()
+            formData.append("myfile", fileInput.files[0])
+            formData.append(uploadConfig.name, fileInput.files[0])
             // formData.append("object", "product")
             // 如果需要token且存在token
-            // if (uploadConfig.token) {
-            //   formData.append("token", uploadConfig.token)
-            // }
+            if (uploadConfig.token) {
+              formData.append("token", uploadConfig.token)
+            }
             // 图片上传
-            // var xhr = new XMLHttpRequest()
-            // xhr.open(uploadConfig.methods, uploadConfig.action, true)
-            // // 上传数据成功，会触发
-            // xhr.onload = function(e) {
-            //   if (xhr.status === 200) {
-            //     var res = JSON.parse(xhr.responseText)
-            //     let length = self.quill.getSelection(true).index
-            //     //这里很重要，你图片上传成功后，img的src需要在这里添加，res.path就是你服务器返回的图片链接。
-            //     self.quill.insertEmbed(length, "image", res.path)
-            //     self.quill.setSelection(length + 1)
-            //   }
-            //   fileInput.value = ""
-            // }
-            // // 开始上传数据
-            // xhr.upload.onloadstart = function(e) {
-            //   fileInput.value = ""
-            // }
-            // // 当发生网络异常的时候会触发，如果上传数据的过程还未结束
-            // xhr.upload.onerror = function(e) {}
-            // // 上传数据完成（成功或者失败）时会触发
-            // xhr.upload.onloadend = function(e) {
-            //   // console.log('上传结束')
-            // }
-            // xhr.send(formData)
+            let xhr = new XMLHttpRequest()
+            xhr.open(uploadConfig.methods, uploadConfig.action, true)
+            // 上传数据成功，会触发
+            xhr.onload = function(e) {
+              if (xhr.status === 200) {
+                // console.log("上传数据成功，会触发")
+                var res = JSON.parse(xhr.responseText)
+                // console.log(res)
+                let length = self.quill.getSelection(true).index
+                // 这里很重要，你图片上传成功后，img的src需要在这里添加，res.path就是你服务器返回的图片链接。
+                self.quill.insertEmbed(length, "image", res.data.url)
+                self.quill.setSelection(length + 1)
+              }
+              fileInput.value = ""
+            }
+            // 开始上传数据
+            xhr.upload.onloadstart = function(e) {
+              // console.log("开始上传数据")
+              fileInput.value = ""
+            }
+            // 当发生网络异常的时候会触发，如果上传数据的过程还未结束
+            xhr.upload.onerror = function(e) {
+              // console.log("当发生网络异常的时候会触发，如果上传数据的过程还未结束")
+              alert("上传文件失败")
+            }
+            // 上传数据完成（成功或者失败）时会触发
+            xhr.upload.onloadend = function(e) {
+              // console.log("上传结束")
+            }
+            xhr.send(formData)
           })
-          // this.container.appendChild(fileInput)
+          this.container.appendChild(fileInput)
         }
         fileInput.click()
       }
