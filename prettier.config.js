aee22e52652f7379acda42cf96f3347d801b840a
xuyangyang2019 <15268296911@163.com>
2020-08-13 18:41:27 +0800

    箭头函数一律显示小括号

--- prettier.config.js
+++ prettier.config.js
@@ -27,7 +27,7 @@ module.exports = {
   // 箭头函数参数括号 默认avoid 可选 avoid| always
   // avoid 能省略括号的时候就省略 例如x => x
   // always 总是有括号
-  arrowParens: "avoid"
+  arrowParens: "always"
   // parser: "babel",
   // printWidth: 120
   // endOfLine: "lf"
