d644dc5a2741d908d4e10da263961cca89d30789
xuyangyang2019 <15268296911@163.com>
2020-08-14 11:49:24 +0800

    自定义toast

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 
-import filters from "./filters"
-
-Vue.config.productionTip = false
-
 // 导入api接口
 import api from "./api"
+import filters from "./filters"
+import Toast from "./components/toast/index"
+
+Vue.config.productionTip = false
 // 将api挂载到vue的原型上复制代码
 Vue.prototype.$api = api
+Vue.use(Toast)
+// Vue.prototype.$toast = Toast
 
 // 全局过滤器
-Object.keys(filters).forEach(filterName => {
+Object.keys(filters).forEach((filterName) => {
   Vue.filter(filterName, filters[filterName])
 })
 
@@ -24,5 +26,5 @@ Object.keys(filters).forEach(filterName => {
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount("#app")
