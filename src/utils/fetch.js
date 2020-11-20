093d1aad83328eabe0208a995e0af4b2a856dbbe
xuyangyang2019 <15268296911@163.com>
2020-11-20 20:53:50 +0800

    后端如果用koa-jwt

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -114,7 +114,9 @@ httpInstance.interceptors.request.use(
     // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码
     // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
     const token = localStorage.getItem("validateToken")
-    token && (config.headers.Authorization = token)
+    // token && (config.headers.Authorization = token)
+    // 如果后端使用的是koa-jwt鉴权
+    token && (config.headers.Authorization = "Bearer " + token)
     return config
   },
   (error) => {
