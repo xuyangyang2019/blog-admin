79e1b8a6be9222c8fdbf6a136aaa5b3b4f023841
xuyangyang2019 <15268296911@163.com>
2021-01-12 16:40:31 +0800

    获取留言、评论的接口

--- src/api/admin.js
+++ src/api/admin.js
@@ -7,9 +7,6 @@
 import fetch from '@/utils/fetch'
 // import { generateTaskId } from '@/utils/util'
 
-/* Common */
-// export const queryUsers = data => Request.get('/api/products', data)
-
 /**
  * 管理员登陆
  * @param {string} username 用户名
@@ -30,4 +27,43 @@ function getArticleList(pageNum, pageSize, publish, tag) {
   return fetch.get('/api/getArticleList', { pageNum: pageNum, pageSize: pageSize, publish: publish, tag: tag })
 }
 
-export { adminLogin, getArticleList }
+// 获取对应模块的文章总数，为分页按钮个数提供支持
+// GetArticlesCount({ commit }, payload) {
+//   return fetch.get("/api/getCount", payload).then((data) => {
+//     commit("PAGE_ARRAY", data)
+//   })
+// },
+
+/**
+ * 获取留言
+ * @param {number} pageNum 第几页
+ * @param {number} pageSize 每页的文章数量
+ */
+function getMsgBoard(pageNum, pageSize) {
+  return fetch.get('/api/getMsgBoard', { pageNum: pageNum, pageSize: pageSize })
+}
+
+// // 获取留言数量
+// GetMsgCount({ commit }) {
+//   return fetch.get('/api/getMsgCount').then((res) => {
+//     commit('PAGE_ARRAY', res.data.count || 0)
+//   })
+// },
+
+/**
+ * 获取评论列表
+ * @param {number} pageNum 第几页
+ * @param {number} pageSize 每页的文章数量
+ */
+function getCommentsList(pageNum, pageSize) {
+  return fetch.get('/api/getCommentsList', { pageNum: pageNum, pageSize: pageSize })
+}
+
+// 获取评论数
+// GetCommentsCount({ commit }) {
+//   return fetch.get("/api/getCommentsCount").then((data) => {
+//     commit("PAGE_ARRAY", data)
+//   })
+// },
+
+export { adminLogin, getArticleList, getMsgBoard, getCommentsList }
