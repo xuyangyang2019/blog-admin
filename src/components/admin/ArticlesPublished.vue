83d927daeba8cdb9a4dc7a8648294fd66e9a5e14
xuyangyang2019 <15268296911@163.com>
2021-08-02 11:46:32 +0800

    重写分页逻辑和ui

--- src/components/admin/ArticlesPublished.vue
+++ src/components/admin/ArticlesPublished.vue
@@ -1,42 +1,73 @@
 <template>
   <div class="admin-articles">
-    <article-list :articleList="articles"></article-list>
+    <ArticleList :articleList="articlesPublished" />
+    <el-pagination
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="articlesCount"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import { getArticlesList, getArticlesCount } from '../../api/admin'
-
 import ArticleList from '../common/ArticleList.vue'
+import { getArticlesList, getArticlesCount } from '../../api/admin'
 
 export default {
   name: 'ArticlesPublished',
   components: {
     ArticleList
   },
-  computed: {
-    ...mapState('admin', {
-      articles: 'articles'
-    })
+  data() {
+    return {
+      articlesPublished: [], // 已发表的文章
+      currentPage: 1, // 当前页
+      pageSize: 10, // 每页文章数
+      articlesCount: 0 // 文章总数
+    }
   },
   created() {
-    // 分页获取文章
-    // this.queryArticles()
+    this.queryArticles()
   },
   methods: {
-    // 查询已发表的文章列表
+    // 查询已发表的文章列表和文章数量
     queryArticles() {
-      getArticlesList(1, 10, true, '').then((res) => {
-        if (res.code === 200) {
-          this.$store.commit('admin/SET_ARTICLES', res.data)
-        }
-      })
+      this.queryArticlesList()
+      // 查询文章数量
       getArticlesCount(true, '').then((res) => {
         if (res.code === 200) {
-          this.$store.commit('admin/SET_PAGE_ARRAY', res.data.count)
+          this.articlesCount = res.data.count
         }
       })
+    },
+    // 查询文章列表
+    queryArticlesList() {
+      getArticlesList(this.currentPage, this.pageSize, true, '')
+        .then((res) => {
+          if (res.code === 200) {
+            this.articlesPublished = res.data
+          } else {
+            this.articlesPublished = []
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+          this.articlesPublished = []
+        })
+    },
+    // pageSize改变
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.queryArticlesList()
+    },
+    // page改变
+    handleCurrentChange(val) {
+      this.currentPage = val
+      this.queryArticlesList()
     }
   },
   // 组件缓存后，为了让每个模块显示正确的页码，故重新计算页码数
