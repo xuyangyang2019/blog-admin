4483830cb27d970308569c58a63114d8dfde6fda
xuyangyang2019 <15268296911@163.com>
2021-08-03 11:33:32 +0800

    添加搜索

--- src/components/admin/ArticlesPublished.vue
+++ src/components/admin/ArticlesPublished.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="admin-articles">
+  <div class="articles-published">
+    <!-- 搜索 -->
+    <div class="search-box">
+      <el-input
+        v-model="keyword"
+        style="width: 200px; margin-right: 10px"
+        placeholder="请输入关键词"
+        size="small"
+        clearable
+        maxlength="10"
+        show-word-limit
+      ></el-input>
+      <el-date-picker
+        v-model="searchTime"
+        style="width: 300px; margin-right: 10px"
+        type="daterange"
+        size="small"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+      ></el-date-picker>
+      <el-button type="primary" size="small" @click="searchArticle">搜索</el-button>
+    </div>
+    <!-- 数据展示 -->
     <ArticleList :articleList="articlesPublished" />
     <!-- 分页 -->
     <el-pagination
+      style="padding:5px 0"
       :current-page="currentPage"
       :page-sizes="[10, 20, 30, 40]"
       :page-size="pageSize"
@@ -26,6 +50,8 @@ export default {
   },
   data() {
     return {
+      keyword: '', // 关键词
+      searchTime: '',
       articlesPublished: [], // 已发表的文章
       currentPage: 1, // 当前页
       pageSize: 10, // 每页文章数
@@ -36,6 +62,12 @@ export default {
   //   this.queryArticles()
   // },
   methods: {
+    // 搜索文章
+    searchArticle() {
+      console.log(this.keyword)
+      console.log(this.searchTime)
+      // this.$router.push({ name: 'search', params: { keyword: this.keyword, searchTime: this.searchTime } })
+    },
     // 查询已发表的文章列表和文章数量
     queryArticles() {
       this.queryArticlesList()
@@ -82,9 +114,15 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.admin-articles {
-  margin-top: 15px;
+<style lang="scss" scoped>
+.articles-published {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .search-box {
+    display: flex;
+    padding: 5px 0;
+  }
   // .fade-enter-active,
   // .fade-leave-active {
   //   transition: opacity 0.3s;
